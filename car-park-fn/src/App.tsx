681b205a442ed81bbfdb1f82f2442295ef6e585a
lwstr0410 <lwstr0410@gmail.com>
2023-08-05 16:04:43 +0800

    nav bar styled

--- car-park-fn/src/App.tsx
+++ car-park-fn/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import CarList from './features/dashboard/CarList';
+import Sidebar from './features/sidebar/Sidebar';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 
 function App() {
   return (
     <div className='App'>
+      <Sidebar />
       <CarList />
     </div>
   );
---
+++ car-park-fn/src/features/sidebar/Sidebar.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faCalendarDays, faMoon } from '@fortawesome/free-regular-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+
+const drawerWidth = 240;
+
+const MySidebar = (props: any) => {return(
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed" elevation={0}
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}>
+        <Toolbar>
+          <Typography className="navbarCarparkName" variant="h6" noWrap component="div">
+            庇利街停車場
+          </Typography>
+          <div className='navbarButtons'>
+          <FontAwesomeIcon icon={faMoon} />
+          <FontAwesomeIcon icon={faPhone} />
+          <FontAwesomeIcon icon={faCalendarDays} />
+          <FontAwesomeIcon icon={faBell} />
+          <div className='staffProfile'>俊</div>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        <Toolbar />
+         </Box>
+         </Box>
+)};
+
+export default MySidebar
