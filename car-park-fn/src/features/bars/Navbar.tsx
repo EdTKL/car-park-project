04f4a69ff9540720e73d8f5c87a86bfc3efdc742
kmng <ngkm.tec@gmail.com>
2023-08-21 14:17:35 +0800

    added pdf DL

--- car-park-fn/src/features/bars/Navbar.tsx
+++ car-park-fn/src/features/bars/Navbar.tsx
@@ -11,40 +11,86 @@ import "./Navbar.scss";
 import { useSelector } from 'react-redux';
 import { RootState } from '../../app/store';
 
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import DownloadPDF from '../../pages/Report/DownloadPDF';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+
 const Navbar = () => {
-    const drawerWidth = useSelector((state: RootState)=> { return state.drawerState.drawerWidth});
-    const open = useSelector((state: RootState)=> { return state.drawerState.open});
+  const drawerWidth = useSelector((state: RootState) => { return state.drawerState.drawerWidth });
+  const open = useSelector((state: RootState) => { return state.drawerState.open });
 
-    interface AppBarProps extends MuiAppBarProps {
-      open?: boolean | any;
-    }
+  interface AppBarProps extends MuiAppBarProps {
+    open?: boolean | any;
+  }
 
-    const AppBar = styled(MuiAppBar, {
-      shouldForwardProp: (prop) => prop !== 'open',
-        })<AppBarProps>(({ theme, open }) => ({
-          width: `calc(100% - ${drawerWidth}px)`,
-          transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-          }),
-          ...(open && {
-            marginLeft: drawerWidth,
-            width: `calc(100% - ${drawerWidth}px)`,
-            transition: theme.transitions.create(['width', 'margin'], {
-              easing: theme.transitions.easing.sharp,
-              duration: theme.transitions.duration.enteringScreen,
-            }),
-          }),
-        }));
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })<AppBarProps>(({ theme, open }) => ({
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }));
 
-    return (
-        <>
-        <AppBar position="fixed" open={open} elevation={0}>
+  const [openD, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <AppBar position="fixed" open={open} elevation={0}>
         <Toolbar>
           <Typography className="navbarCarparkName" variant="h6" noWrap component="div">
             庇利街停車場
           </Typography>
           <div className='navbarButtons'>
+
+
+          <PictureAsPdfIcon onClick={handleClickOpen} />
+            <Dialog
+              open={openD}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle sx={{ fontWeight: 700 }} id="alert-dialog-title">
+                {"PDF檔案"}
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  金偉停車場出入記錄
+                  （近三個月）
+                </DialogContentText>
+                <DownloadPDF />
+              </DialogContent>
+              <DialogActions>
+                {/* <Button onClick={handleClose}>Disagree</Button> */}
+                <Button onClick={handleClose} autoFocus>
+                  關閉
+                </Button>
+              </DialogActions>
+            </Dialog>
+
             <DarkModeOutlinedIcon />
             <LocalPhoneOutlinedIcon />
             <CalendarMonthOutlinedIcon />
@@ -52,7 +98,7 @@ const Navbar = () => {
             <Avatar className='staffProfile'>俊</Avatar>
           </div>
         </Toolbar>
-    </AppBar></>)
+      </AppBar></>)
 
 }
 export default Navbar
---
+++ car-park-fn/src/pages/Report/DownloadPDF.tsx
@@ -0,0 +1,206 @@
+import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import { PDFDownloadLink, Page, Text, Document, StyleSheet, View, Font } from '@react-pdf/renderer';
+import { Car } from '../../features/models';
+
+import NotoSan from './NotoSansTC-Regular.otf'
+
+// import 'reactjs-popup/dist/index.css';
+
+// import "./DownloadPDF.css";
+
+const fetchData = async () => {
+
+  const response = await fetch(`${process.env.REACT_APP_API_BASE}carpark/getReadTable`);
+  const result = await response.json();
+  return result.data as Car[]
+};
+console.log(fetchData)
+
+Font.register({
+  family: 'NotoSan',
+  src: NotoSan
+});
+
+const re = /T/gi;
+
+const PdfDocument = ({ data }: { data: any }) => (
+
+  <Document>
+    <Page style={styles.page} orientation="landscape" size="A4">
+      <Text style={styles.title}>時租交易記錄</Text>
+      <View style={styles.table}>
+        <View style={styles.tableRow}>
+          <Text style={styles.tableHeaderCellLeft}>收據編號</Text>
+          <Text style={styles.columnLessThin}>次序</Text>
+          <Text style={styles.tableHeaderCell}>車牌號碼</Text>
+          <Text style={styles.tableHeaderCell}>車類</Text>
+          <Text style={styles.tableHeaderCell}>泊車日期&時間</Text>
+          <Text style={styles.tableHeaderCell}>出車日期&時間</Text>
+          <Text style={styles.columnLessThin}>總時數</Text>
+          <Text style={styles.columnThin}>時</Text>
+          <Text style={styles.columnThin}>日</Text>
+          <Text style={styles.columnThin}>夜</Text>
+          <Text style={styles.columnThin}>24</Text>
+          <Text style={styles.columnThin}>全</Text>
+          <Text style={styles.columnThin}>失</Text>
+          <Text style={styles.columnThin}>+/-</Text>
+          <Text style={styles.tableHeaderCell}>總金額</Text>
+          <Text style={styles.tableHeaderCell}>狀況</Text>
+          <Text style={styles.tableHeaderCellRight}>職員編號</Text>
+        </View>
+
+        {data.map((car: any, idx: number) => (
+
+          <View break={(idx === 14) || ((idx + 2) % 16 === 0 && idx >= 14)} key={car.id} style={styles.tableRow}>
+
+            <Text style={styles.tableCellLeft}>{car.invoice_num}</Text>
+            <Text style={styles.columnLessThin}>{car.id}</Text>
+            <Text style={styles.tableCell}>{car.plate_num}</Text>
+            <Text style={styles.tableCell}>{car.vehicle_type === "small_car" ? "小型車" : "電單車"}</Text>
+            <Text style={styles.tableCell}>{car.entry_time.slice(0, -5).replace(re, " ")}</Text>
+            <Text style={styles.tableCell}>{car.exit_time.slice(0, -5).replace(re, " ")}</Text>
+            <Text style={styles.columnLessThin}>{car.total_hours}</Text>
+            <Text style={styles.columnThin}>{car.parked_hours}</Text>
+            <Text style={styles.columnThin}>{car.parked_days}</Text>
+            <Text style={styles.columnThin}>{car.parked_nights}</Text>
+            <Text style={styles.columnThin}>{car.id}</Text>
+            <Text style={styles.columnThin}>{car.id}</Text>
+            <Text style={styles.columnThin}>{car.id}</Text>
+            <Text style={styles.columnThin}>{car.id}</Text>
+            <Text style={styles.tableCell}>{car.payment}</Text>
+            <Text style={styles.tableCell}>{car.status === "left" ? "已出車" : "/"}</Text>
+            <Text style={styles.tableCellRight}>{car.staff_id}</Text>
+
+          </View>
+
+
+        ))}
+      </View>
+    </Page>
+  </Document>
+);
+
+const styles = StyleSheet.create({
+  page: {
+    fontFamily: 'NotoSan',
+    fontSize: "8px",
+    padding: '1cm',
+    width: '100%',
+  },
+
+  title: {
+    fontSize: 18,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  table: {
+    display: 'flex',
+    flexDirection: 'column',
+    // borderLeftWidth: 2,
+    // borderRightWidth: 1,
+    // borderTopWidth: 2,
+    // borderBottomWidth: 1,
+  },
+  tableRow: {
+    flexDirection: 'row',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: '#555',
+  },
+  tableHeaderCell: {
+    backgroundColor: '#f2f2f2',
+    padding: 4,
+    flex: 1,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    borderRightWidth: 0.5,
+    borderColor: '#555',
+  },
+  tableHeaderCellLeft: {
+    backgroundColor: '#f2f2f2',
+    padding: 4,
+    flex: 1,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    borderLeftWidth: 1,
+    borderRightWidth: 0.5,
+    borderColor: '#555',
+  },
+  tableHeaderCellRight: {
+    backgroundColor: '#f2f2f2',
+    padding: 4,
+    flex: 1,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    borderLeftWidth: 0.5,
+    borderRightWidth: 1,
+    borderColor: '#555',
+  },
+  tableCell: {
+    padding: 4,
+    flex: 1,
+    textAlign: 'center',
+    borderRightWidth: 0.5,
+    borderColor: '#555',
+  },
+  tableCellLeft: {
+    padding: 4,
+    flex: 1,
+    textAlign: 'center',
+    borderLeftWidth: 1,
+    borderRightWidth: 0.5,
+    borderColor: '#555',
+  },
+  tableCellRight: {
+    padding: 4,
+    flex: 1,
+    textAlign: 'center',
+    borderLeftWidth: 0.5,
+    borderRightWidth: 1,
+    borderColor: '#555',
+  },
+  columnThin: {
+    padding: 4,
+    // flex: 1,
+    textAlign: 'center',
+    borderRightWidth: 0.5,
+    borderColor: '#555',
+    // width:'10px',
+    width: "24px"
+  },
+  columnLessThin: {
+    padding: 4,
+    // flex: 1,
+    textAlign: 'center',
+    borderRightWidth: 0.5,
+    borderColor: '#555',
+    // width:'10px',
+    width: "40px"
+  },
+});
+
+const DownloadPDF = () => {
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["carList"],
+    queryFn: fetchData
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching data</div>;
+  }
+  console.log(data)
+  return (
+    <div>
+      <PDFDownloadLink document={<PdfDocument data={data} />} fileName="car-data.pdf">
+        {({ blob, url, loading, error }) => (loading ? '載入中...' : '點擊下載')}
+      </PDFDownloadLink>
+    </div>
+  );
+};
+
+export default DownloadPDF;
